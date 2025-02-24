d47800e519399cc5367d3a5115992fe2395e973d
MW <59529458+witmarinf@users.noreply.github.com>
2025-02-24 21:48:15 +0100

    order by ID

---
+++ Procesy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessConsoleApp
+{
+    class Procesy
+    {
+        public int Id { get; set; }
+
+        public string ProcessName { get; set; }
+
+        public int PrivateMemorySize64 { get; set; }
+    
+        public Procesy(int id, string processName, int privateMemorySize64)
+        {
+            Id = id;
+            ProcessName = processName;
+            PrivateMemorySize64 = privateMemorySize64;
+        }
+    }
+
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -14,17 +14,34 @@ namespace ProcessConsoleApp
             Console.WriteLine("Lista aktualnie uruchomionych procesów:");
 
             Process[] processes = Process.GetProcesses();
+            List<Procesy> procList = new List<Procesy>();
 
             foreach (Process process in processes)
             {
                 try
                 {
-                    Console.WriteLine($"ID: {process.Id} | Nazwa: {process.ProcessName} | Pamięć: {process.PrivateMemorySize64 / 1024} KB");
+                    //Console.WriteLine($"ID: {process.Id} | Nazwa: {process.ProcessName} | Pamięć: {process.PrivateMemorySize64 / 1024} KB");
+
+                    procList.Add(new Procesy(process.Id, process.ProcessName, (int)process.PrivateMemorySize64));
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine($"Nie można uzyskać informacji o procesie: {ex.Message}");
                 }
+            }
+
+            foreach (Procesy item in procList) 
+            {
+                Console.WriteLine($"{item.Id} | {item.ProcessName} | {item.PrivateMemorySize64/1024} KB");
+            }
+
+            List<Procesy> dataProc = procList;
+
+            var listOrderById = dataProc.OrderBy(x => x.Id);
+
+            foreach (Procesy item in listOrderById)
+            {
+                Console.WriteLine($"{item.Id} | {item.ProcessName} | {item.PrivateMemorySize64 / 1024} KB");
             }
 
         }
